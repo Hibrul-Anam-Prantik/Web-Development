94f3310ad598db17278cb307e675677319175d79
Hibrul-Anam-Prantik <hibrul.anam.prantik@g.bracu.ac.bd>
2024-11-02 11:47:35 +0600

    Class 01

---
+++ JS/Class01.js
@@ -0,0 +1,15 @@
+// Variables: var, let, const
+// const can't be changed later
+// print: console.log(); output
+let y = 30;
+y = 40;
+console.log(y);
+let x = 20;
+if(true) {
+    let x = 25;
+    console.log(x); // output 20
+}
+console.log(x);
+
+// const arr = [1,2,3,4, "4343"];
+//     arr = "String";
